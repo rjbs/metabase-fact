6e479376daf895efeeb7ae455db6497559a3836a
David Golden <dagolden@cpan.org>
2009-03-28 15:20:08 +0000

    add Hash and update String in anticipation of new serialization

--- lib/CPAN/Metabase/Fact/Hash.pm
+++ lib/CPAN/Metabase/Fact/Hash.pm
@@ -1,14 +1,16 @@
-package CPAN::Metabase::Fact::String;
+package CPAN::Metabase::Fact::Hash;
 use base 'CPAN::Metabase::Fact';
+
+use Carp ();
 
 sub content_as_struct {
   my ($self) = @_;
-  return { string => $self->content };
+  return $self->content;
 }
 
 sub content_from_struct { 
   my ($class, $struct) = @_;
-  return $struct->{string} || '';
+  return $stuct;
 }
 
 1;
