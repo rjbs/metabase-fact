7cc9085f72e83ebebc237f9fdb3f038cc75f3ca7
David Golden <dagolden@cpan.org>
2009-03-29 10:23:09 +0100

    documentation and minor test fix

--- t/lib/FactSubclasses.pm
+++ t/lib/FactSubclasses.pm
@@ -14,13 +14,23 @@ sub content_from_bytes  { return reverse($_[1])             };
 
 package FactThree;
 use base 'CPAN::Metabase::Fact::String';
-sub validate_content    { length $_[0]->content > 0 }
-sub content_metadata    { return { 'length' => [ 'Num' => length $_[0]->content ] } }
+sub validate_content    { 
+  $_[0]->SUPER::validate_content;
+  die "content not positive length" unless length $_[0]->content > 0;
+}
+sub content_metadata    { 
+  return { 'length' => [ 'Num' => length $_[0]->content ] } 
+}
 
 package FactFour;
 use base 'CPAN::Metabase::Fact::Hash';
-sub validate_content    { ref $_[0]->content eq 'HASH' or die "not a hash-ref" }
-sub content_metadata    { return { 'size' => [ Num => scalar keys %{ $_[0]->content } ] } }
+sub validate_content    { 
+  $_[0]->SUPER::validate_content;
+  die "not a hash-ref" unless ref $_[0]->content eq 'HASH'; 
+}
+sub content_metadata    { 
+  return { 'size' => [ Num => scalar keys %{ $_[0]->content } ] } 
+}
 
 
 1;
