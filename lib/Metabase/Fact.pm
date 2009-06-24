421eaa10a327dbd0b279e407cbaf02b0732307d2
Ricardo SIGNES <rjbs@cpan.org>
2009-06-24 16:08:18 -0400

    remove confusing duplicate (c) notice

--- lib/Metabase/Fact.pm
+++ lib/Metabase/Fact.pm
@@ -1,9 +1,3 @@
-# Copyright (c) 2008 by Ricardo Signes. All rights reserved.
-# Licensed under terms of Perl itself (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a 
-# copy of the License from http://dev.perl.org/licenses/
-
 package Metabase::Fact;
 use 5.006;
 use strict;
--- lib/Metabase/User/Secret.pm
+++ lib/Metabase/User/Secret.pm
@@ -1,9 +1,3 @@
-# Copyright (c) 2009 by David Golden. All rights reserved.
-# Licensed under terms of Perl itself (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a 
-# copy of the License from http://dev.perl.org/licenses/
-
 package Metabase::User::Secret;
 use 5.006;
 use strict;
