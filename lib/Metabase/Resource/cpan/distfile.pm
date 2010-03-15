54d6e229b63113ee82e287873d64b7ced8d84829
David Golden <dagolden@cpan.org>
2010-03-15 02:25:30 -0400

    don't auto-vivify complex data structures with my

--- lib/Metabase/Resource/cpan/distfile.pm
+++ lib/Metabase/Resource/cpan/distfile.pm
@@ -60,7 +60,7 @@ sub _validate_distfile {
   my $d = eval { CPAN::DistnameInfo->new($path) };
   my $bad = defined $d ? 0 : 1;
 
-  my $cache->{dist_file} = $string;
+  my $cache = { dist_file => $string };
 
   for my $k ( $bad ? () : (keys %distfile_map) ) {
     my $value = $d->$k;
